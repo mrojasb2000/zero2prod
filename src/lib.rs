dbd4edadbf6d6cd5a112fa3a307d9537648bf0e5
mavro <mrojasb2000@gmail.com>
2024-01-20 21:16:55 -0300

    feat: adding first unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
 //! lib.rs
-use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 //noinspection RsMainFunctionNotFound
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/healthcheck", web::get().to(health_check))
-    })
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/healthcheck", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,39 @@
-use zero2prod::main;
+//! tests/health_check.rs
 
-#[test]
-fn dummy_test() {
-    main()
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app(); //.await.expect("Failed to spawn our app.");
+
+    // We need to bring in `request`
+    // to perform HTTP requests against our application
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/healthcheck")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background ~somehow~
+
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hance the non-binding let
+    let _ = tokio::spawn(server);
 }
